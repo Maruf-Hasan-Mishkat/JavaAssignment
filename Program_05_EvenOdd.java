6b7c9e6cf8a501e14aae577aded193aae7b368b1
Maruf-Hasan-Mishkat <marufhasanmishkat@gmail.com>
2026-08-07 16:42:58 +0600

    Add Program_05

---
+++ Program_05_EvenOdd.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Program_05_EvenOdd {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even.");
+        } 
+        else {
+            System.out.println(number + " is Odd.");
+        }
+
+
+    }
+
+}
